30eaef323a19e0fd2853c370df4dcda4beb83cf5
RodrigoOFernandes <rodrigofernan132004@gmail.com>
2025-03-05 10:23:34 +0000

    update survival status

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,15 @@
 use rand::random_bool;
 
-struct Point {
-    x: u8,
-    y: u8,
-}
-
 struct Cell {
     alive: bool,
-    position: Point,
 }
 
 fn create_cells(max: u8) -> Vec<Cell> {
     let mut cells: Vec<Cell> = Vec::new(); 
-    for i in 0..max {
-        for j in 0..max {
+    for _i in 0..max {
+        for _j in 0..max {
             let cell = Cell {
                 alive: random_bool(0.5),
-                position: Point { x: i, y: j},
             };
             cells.push(cell);
         }
@@ -24,12 +17,54 @@ fn create_cells(max: u8) -> Vec<Cell> {
     cells
 }
 
+fn get_neighbors(index: usize, max: u8) -> Vec<usize> {
+    let i = index / max as usize;
+    let j = index % max as usize;
+
+    let mut neighbors = Vec::new();
+
+    for di in -1..=1 {
+        for dj in -1..=1 {
+            if di == 0 && dj == 0 {
+                continue;
+            }
+            
+            let ni = i as isize + di;
+            let nj = j as isize + dj;
+
+            if ni >= 0 && ni < max as isize && nj >= 0 && nj < max as isize {
+                let neighbor_index = ni as usize * max as usize +nj as usize;
+
+                neighbors.push(neighbor_index);
+            }
+        }
+    }
+    
+    neighbors
+}
+
+fn survivors(mut cells: Vec<Cell>, max: u8) -> Vec<Cell> {
+    for i in 0..cells.len(){
+        let neighbors = get_neighbors(i, max);
+
+        let count = neighbors.iter().filter(|&&idx| cells[idx].alive == true).count();
+    
+        if (count == 3) || (count == 2 && cells[i].alive == true)  {
+            cells[i].alive = true;
+        } else {
+            cells[i].alive = false;
+        }
+    }
+    cells
+}
+
 fn main() {
-    let cells = create_cells(10);
+    let max: u8 = 10;
+    let cells = create_cells(max);
     let mut count: u8 = 0;
 
     for cell in cells {
-        if count % 9 == 0 {
+        if count % max == 0 {
             print!("\n");
         }
         if cell.alive == true { 
@@ -39,4 +74,5 @@ fn main() {
         }
         count += 1;
     }
+    print!("\n");
 }
