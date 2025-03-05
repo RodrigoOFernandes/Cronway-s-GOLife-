d363566818b696802e281f0892fc0648c1064142
RodrigoOFernandes <rodrigofernan132004@gmail.com>
2025-03-05 09:29:00 +0000

    start

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use rand::{random, random_bool};
+use rand::random_bool;
 
 struct Point {
     x: u8,
@@ -12,17 +12,31 @@ struct Cell {
 
 fn create_cells(max: u8) -> Vec<Cell> {
     let mut cells: Vec<Cell> = Vec::new(); 
-    for _i in 0..max {
-        let cell = Cell {
-            alive: random_bool(0.5),
-            position: Point { x: random::<u8>(), y: random::<u8>()},
-        };
-        cells.push(cell);
+    for i in 0..max {
+        for j in 0..max {
+            let cell = Cell {
+                alive: random_bool(0.5),
+                position: Point { x: i, y: j},
+            };
+            cells.push(cell);
+        }
     }
     cells
 }
 
 fn main() {
     let cells = create_cells(10);
+    let mut count: u8 = 0;
 
+    for cell in cells {
+        if count % 9 == 0 {
+            print!("\n");
+        }
+        if cell.alive == true { 
+            print!("#")
+        } else {
+            print!("%")
+        }
+        count += 1;
+    }
 }
